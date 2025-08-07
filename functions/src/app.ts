f118444feb8d6c0b48c240b9cd91bbdfcb1ca49b
Franklin-CortesS <franklin.cortesserrano@gmail.com>
2025-08-07 06:21:55 -0500

    FC-enable cors

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,8 +1,27 @@
 import express from "express";
+import cors from "cors";
 import tareasRouter from "./routes/tasks.routes";
 import authRouter from "./routes/auth.routes";
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:4200",
+  "https://todoapp-fcs2499.web.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy: Origin not allowed"));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use("/tasks", tareasRouter);
 app.use("/auth", authRouter);
